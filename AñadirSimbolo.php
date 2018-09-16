ff659ce93bce548250d79944591b6f68dfe3e585
JonathanLopez <jackobblackjr@gmail.com>
2018-09-16 14:08:34 -0500

    Añadir simbolos parte 1

---
+++ AñadirSimbolo.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<!--TablaDeSimbolos JS-->
+        <meta charset="utf-8" />
+
+    <head>
+        <title>Añadir simbolo</title>
+        <center>    
+            <br><br><br>
+            
+      
+      <body background="fondo.jpg" style="background-repeat:no-repeat"> 
+        <form action="guardar.php" method="POST">
+            <font color="white"><h2>Identificador:</h2></font>
+            <input type="text" REQUIRED name="Identificador" value="">
+            <font color="white"><h2>Tipo:</h2></font>
+            <input type="text" REQUIRED name="Tipo" value="">
+            <font color="white"><h2>Dimension:</h2></font>
+            <input type="text" REQUIRED name="Dimension" value="">
+            <font color="white"><h2>Declarada en:</h2></font>
+            <input type="text" REQUIRED name="Declarada" value="">
+            <font color="white"><h2>Referencias:</h2></font>
+            <input type="text" REQUIRED name="Referencias" value=""><br><br>
+            <input type="submit" value="Aceptar">
+
+
+        </form>
+    </body>
+
+</html>
